715fafa76e22391ef0beb83a939a2afa4c3486ae
zhongshuai520 <879816303@example.com>
2026-05-02 14:27:08 +0800

    更新vite配置

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',  // 添加这一行
   server: {
+    host: '0.0.0.0',
     port: 5173,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
